68bed2e5fbc6710e42e003938f2cfebdf0f9bf6f
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2023-12-28 11:01:38 +0100

    added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,44 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      o9Sans: ['o9Sans', 'sans-serif'],
+      'o9Sans-bold': ['o9Sans-bold', 'sans-serif'],
+      'o9Sans-light': ['o9Sans-light', 'sans-serif'],
+    },
+    screens: {
+      desktopL: '1920px',
+      // => @media(min-width: 1920px)
+      desktop: '1280px',
+      // => @media(min-width: 1290px)
+      tablet: '600px',
+      // => @media(min-width: 600px)
+      mobile: '390px',
+      // => @media(min-width: 600px)
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontSize: {
+        xxs: '12px',
+        xs: '14px',
+        base: '17px',
+        s: '20px',
+        xxm: '24px',
+        xm: '32px',
+        m: '40px',
+        lm: '48px',
+        l: '56px',
+        xl: '80px',
+        xxl: '104px',
+      },
+      colors: {
+        black: '#000',
+        white: '#fff',
+        background: ' #E5E5E5',
+        cardBackground: '#DADADA',
+        cardBackgroundDark: '#CACACA',
+        neon: '#D7FE51',
       },
     },
   },
   plugins: [],
-}
+};
