4ce9ce3d2479bdeb5fe68c1da0b1750fac919584
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2023-12-28 12:10:16 +0100

    added typography config

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <main>this is my app</main>;
+  return (
+    <main>
+      <h1>this is h1</h1>
+      <h2>this is h2</h2>
+      <h3>this is h3</h3>
+      <h4>this is h4</h4>
+      <h5>this is h5</h5>
+      <p>this is p</p>
+    </main>
+  );
 }
